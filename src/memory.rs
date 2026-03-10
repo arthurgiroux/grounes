8579571591e1fa4c81724b4ff1b70d12bd2bbcf3
Arthur Giroux <arthurgiroux@gmail.com>
2026-03-09 21:30:09 -0600

    Simplified memory bus trait by only keeping byte operation.

--- src/memory.rs
+++ src/memory.rs
@@ -2,9 +2,7 @@ use std::usize;
 
 pub trait MemoryBus {
     fn read_byte(&self, addr: u16) -> u8;
-    fn read_word(&self, addr: u16) -> u16;
     fn write_byte(&mut self, addr: u16, value: u8);
-    fn write_word(&mut self, addr: u16, value: u16);
 }
 
 pub struct RAM {
@@ -23,18 +21,8 @@ impl MemoryBus for RAM {
     fn read_byte(&self, addr: u16) -> u8 {
         self.memory[addr as usize]
     }
-
-    fn read_word(&self, addr: u16) -> u16 {
-        u16::from_le_bytes([self.memory[addr as usize], self.memory[(addr + 1) as usize]])
-    }
-
+    
     fn write_byte(&mut self, addr: u16, value: u8) {
         self.memory[addr as usize] = value;
-    }
-
-    fn write_word(&mut self, addr: u16, value: u16) {
-        let [low, high] = value.to_le_bytes();
-        self.memory[addr as usize] = low;
-        self.memory[(addr + 1) as usize] = high;
     }
 }
