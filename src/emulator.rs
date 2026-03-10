ffd408f11766e43569b9c6d3ef9afb5cca88ab23
Arthur Giroux <arthurgiroux@gmail.com>
2026-03-09 21:07:42 -0600

    Added memory module and RAM implementation.

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,13 +1,16 @@
 use crate::cpu::CPU;
+use crate::memory::RAM;
 
 pub struct Emulator {
     cpu: CPU,
+    ram: RAM,
 }
 
 impl Emulator {
     pub fn new() -> Emulator {
         Emulator {
             cpu: CPU::default(),
+            ram: RAM::new(2048),
         }
     }
 }
