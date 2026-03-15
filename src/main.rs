858725c0a53ad2a0fd123b264282d8c02095ef29
Arthur Giroux <arthurgiroux@gmail.com>
2026-03-15 15:41:03 -0600

    Working on getting nesttest rom to pass.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ pub mod emulator;
 pub mod ines;
 pub mod memory;
 
+use std::io;
+
 use crate::emulator::Emulator;
 
 fn main() {
     let mut emulator = Emulator::new();
+    if let Err(err) = emulator.load_rom("data/nestest.nes") {
+        println!("Couldn't open ROM, error={}", err);
+        return;
+    }
     emulator.power_up();
+    let mut input = String::new();
+
+    loop {
+        emulator.step();
+        let _ = io::stdin().read_line(&mut input);
+    }
 }
