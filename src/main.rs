44b2a4ea320e3dd4112b116c6b114aef09ad863a
Arthur Giroux <arthurgiroux@gmail.com>
2026-03-23 22:11:45 -0600

    Started implementing simple GUI with egui and eframe.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
-use grounes::emulator::Emulator;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use grounes::emulator::{self, Emulator};
 use std::io;
 
-fn main() {
-    let mut emulator = Emulator::new();
-    if let Err(err) = emulator.load_rom("data/nestest.nes") {
-        println!("Couldn't open ROM, error={}", err);
-        return;
-    }
-    emulator.power_up();
-    let mut input = String::new();
+use eframe::egui;
 
-    loop {
-        emulator.step();
-        let _ = io::stdin().read_line(&mut input);
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Grounes",
+        native_options,
+        Box::new(|cc| Ok(Box::new(GrounesApp::new(cc)))),
+    )
+}
+
+struct GrounesApp {
+    rom_loaded: bool,
+    emulator: Emulator,
+}
+
+impl GrounesApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        GrounesApp {
+            rom_loaded: false,
+            emulator: Emulator::new(),
+        }
+    }
+}
+
+impl eframe::App for GrounesApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if !self.rom_loaded {
+                if ui.button("Load rom").clicked() {
+                    let res = self.emulator.load_rom("data/nestest.nes");
+                    if res.is_ok() {
+                        self.emulator.power_up();
+                        self.rom_loaded = true
+                    }
+                }
+            } else {
+                if ui.button("Step CPU").clicked() {
+                    self.emulator.step();
+                }
+                ui.heading(format!("{:?}", self.emulator.cpu));
+            }
+        });
     }
 }
