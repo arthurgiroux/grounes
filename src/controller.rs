cf08763a94bf6f1c5bb2313ccde60fc654bc5456
Arthur Giroux <arthurgiroux@gmail.com>
2026-09-10 22:48:04 -0600

    Added some tests.

--- src/controller.rs
+++ src/controller.rs
@@ -51,12 +51,63 @@ impl Controller {
     }
 
     pub fn get_latched_value_and_shift(&mut self) -> u8 {
-        if self.shift_value > 8 {
+        if self.shift_value >= 8 {
             1
         } else {
             let ret = (self.latched_value >> self.shift_value) & 0x01;
             self.shift_value += 1;
             ret
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn update_button_state_doesnt_update_latch_if_disabled() {
+        let mut controller = Controller::default();
+        controller.update_latch = false;
+        assert_eq!(controller.latched_value, 0u8);
+        controller.update_button_state(ButtonFlags::all());
+        assert_eq!(controller.latched_value, 0u8);
+    }
+
+    #[test]
+    fn update_button_state_does_update_latch_if_enabled() {
+        let mut controller = Controller::default();
+        assert_eq!(controller.latched_value, 0u8);
+        controller.update_latch = true;
+        controller.update_button_state(ButtonFlags::all());
+        assert_eq!(controller.latched_value, 0b11111111);
+    }
+
+    #[test]
+    fn get_latched_value_and_shift_returns_shifted_value() {
+        let mut controller = Controller::default();
+        controller.update_latch = true;
+        // One every two input
+        controller.update_button_state(
+            ButtonFlags::A | ButtonFlags::Select | ButtonFlags::Up | ButtonFlags::Left,
+        );
+
+        for i in 0..8 {
+            assert_eq!(controller.get_latched_value_and_shift(), (i + 1) % 2);
+        }
+    }
+
+    #[test]
+    fn get_latched_value_and_shift_returns_one_after_all_inputs() {
+        let mut controller = Controller::default();
+
+        for i in 0..20 {
+            assert_eq!(
+                controller.get_latched_value_and_shift(),
+                if (i >= 8) { 1 } else { 0 }
+            );
         }
     }
 }
